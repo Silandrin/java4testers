7698332dd997c3cc414d1d447888fd4ae0ce0c77
Alexey T <xpoh@inbox.ru>
2017-04-28 15:39:11 +0300

    Создан базовый класс для тестов

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/ContactAddingTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/ContactAddingTest.java
@@ -1,30 +1,8 @@
 package ru.stqa.jft.addressbook;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 
-public class ContactAddingTest {
-    ChromeDriver driver;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+public class ContactAddingTest extends TestBase {
 
     @Test
     public void testContactAdding() {
@@ -34,47 +12,4 @@ public class ContactAddingTest {
         returnToHomePage();
     }
 
-    private void returnToHomePage() {
-        driver.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitContactForm() {
-        driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        driver.findElement(By.name("firstname")).click();
-        driver.findElement(By.name("firstname")).clear();
-        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        driver.findElement(By.name("lastname")).click();
-        driver.findElement(By.name("lastname")).clear();
-        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        driver.findElement(By.name("address")).click();
-        driver.findElement(By.name("address")).clear();
-        driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        driver.findElement(By.name("mobile")).click();
-        driver.findElement(By.name("mobile")).clear();
-        driver.findElement(By.name("mobile")).sendKeys(contactData.getMobPhone());
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    private void initContactAdding() {
-        driver.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-    
-    public static boolean isAlertPresent(ChromeDriver driver) {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/GroupCreationTests.java
@@ -1,31 +1,8 @@
 package ru.stqa.jft.addressbook;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 
-public class GroupCreationTests {
-    ChromeDriver driver;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/");
-        login("admin", "secret");
-
-    }
-
-    private void login(String username, String password) {
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
@@ -36,45 +13,4 @@ public class GroupCreationTests {
         returnToGroupPage();
     }
 
-    private void returnToGroupPage() {
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    private void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-    
-    public static boolean isAlertPresent(ChromeDriver driver) {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
