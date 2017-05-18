0660d678b4218d9cd26a5fcde5046f3769278c87
Silandrin <xpoh@inbox.ru>
2017-05-18 07:30:06 +0300

    сгенерированы методы equals

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/model/GroupData.java
@@ -22,4 +22,19 @@ public class GroupData {
     public String getFooter() {
         return footer;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
