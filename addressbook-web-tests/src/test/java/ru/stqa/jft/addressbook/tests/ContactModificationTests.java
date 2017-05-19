137135357f2d8417f4a52a9dc1ec3199ec833aae
Silandrin <xpoh@inbox.ru>
2017-05-19 22:04:26 +0300

    Добавлены отключаемые проверки данных из UI

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactModificationTests.java
@@ -40,5 +40,6 @@ public class ContactModificationTests extends TestBase {
         assertThat(app.contact().count(), equalTo(before.size()));
         Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
+        verifyContactsListInUI();
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/TestBase.java
@@ -4,6 +4,15 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.jft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.jft.addressbook.model.ContactData;
+import ru.stqa.jft.addressbook.model.Contacts;
+import ru.stqa.jft.addressbook.model.GroupData;
+import ru.stqa.jft.addressbook.model.Groups;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class TestBase {
 
@@ -20,4 +29,35 @@ public class TestBase {
         app.stop();
     }
 
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream().map((g) ->
+                    new GroupData()
+                            .withId(g.getId())
+                            .withName(g.getName())
+            ).collect(Collectors.toSet())));
+        }
+    }
+
+    public void verifyContactsListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            Contacts uiContacts = app.contact().all();
+            assertThat(uiContacts, equalTo(dbContacts.stream().map((g) ->
+                    new ContactData()
+                            .withId(g.getId())
+                            .withFirstname(g.getFirstname())
+                            .withLastname(g.getLastname())
+                            .withAddress(g.getAddress())
+                            .withHomePhone(g.getHomePhone())
+                            .withMobPhone(g.getMobPhone())
+                            .withWorkPhone(g.getWorkPhone())
+                            .withEmail(g.getEmail())
+                            .withEmail2(g.getEmail2())
+                            .withEmail3(g.getEmail3())
+            ).collect(Collectors.toSet())));
+        }
+    }
 }
