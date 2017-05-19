caeabb5073bc8a7ecedab75cc96356da3cb7c781
Silandrin <xpoh@inbox.ru>
2017-05-19 06:47:39 +0300

    При создании контакта добавляется фото

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.jft.addressbook.model.ContactData;
 import ru.stqa.jft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,13 +15,15 @@ public class ContactCreationTests extends TestBase {
     public void testContactAdding() {
         app.goTo().contactPage();
         Contacts before = app.contact().all();
+        File photo = new File("src/test/resources/image.png");
         ContactData contact = new ContactData()
                 .withFirstname("Firstname")
                 .withLastname("Lastname")
                 .withAddress("SomeCity, Nether Street, 123")
                 .withMobPhone("+79876543210")
                 .withEmail("nomail@domain.no")
-                .withGroup("test1");
+                .withGroup("test1")
+                .withPhoto(photo);
         app.contact().create(contact, true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
