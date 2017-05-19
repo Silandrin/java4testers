c3297bc9d4c8fe74a16348c4574eaa5bf69ad803
Silandrin <xpoh@inbox.ru>
2017-05-19 09:02:26 +0300

    Параметризованы тесты создания контактов и групп

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,52 @@
 package ru.stqa.jft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.jft.addressbook.model.ContactData;
 import ru.stqa.jft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactAdding() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        File photo = new File("src/test/resources/image.png");
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new ContactData().withFirstname("Firstname 1")
+                                                .withLastname("Lastname 1")
+                                                .withAddress("SomeCity, Nether Street, 1")
+                                                .withMobPhone("+19876543210")
+                                                .withEmail("nomail_1@domain.no")
+                                                .withGroup("test 1")
+                                                .withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstname("Firstname 2")
+                                                .withLastname("Lastname 2")
+                                                .withAddress("SomeCity, Nether Street, 2")
+                                                .withMobPhone("+29876543210")
+                                                .withEmail("nomail_2@domain.no")
+                                                .withGroup("test 2")
+                                                .withPhoto(photo)});
+        list.add(new Object[] {new ContactData().withFirstname("Firstname 3")
+                                                .withLastname("Lastname 3")
+                                                .withAddress("SomeCity, Nether Street, 3")
+                                                .withMobPhone("+39876543210")
+                                                .withEmail("nomail_3@domain.no")
+                                                .withGroup("test 3")
+                                                .withPhoto(photo)});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactAdding(ContactData contact) {
         app.goTo().contactPage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/image.png");
-        ContactData contact = new ContactData()
-                .withFirstname("Firstname")
-                .withLastname("Lastname")
-                .withAddress("SomeCity, Nether Street, 123")
-                .withMobPhone("+79876543210")
-                .withEmail("nomail@domain.no")
-                .withGroup("test1")
-                .withPhoto(photo);
         app.contact().create(contact, true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
